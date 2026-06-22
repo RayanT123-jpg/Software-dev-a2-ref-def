7c788a869f63a4f380ad450755a893f6c69bb233
RayanT123-jpg <rt24aaf@herts.ac.uk>
2026-06-22 21:26:18 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+
+WIDTH = 800
+HEIGHT = 400
+FPS = 60
+
+PLAYER_SPEED = 5
+GRAVITY = 0.5
+JUMP_FORCE = -10
+
+BASE_DIR = os.path.dirname(__file__)
+ASSET_PATH = os.path.join(BASE_DIR, "assets")
