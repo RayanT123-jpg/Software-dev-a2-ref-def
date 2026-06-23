c6259daab9dfe16cee628b28f6b3f177e6d420f3
RayanT123-jpg <rt24aaf@herts.ac.uk>
2026-06-23 21:01:40 +0100

    Update level.py

--- level.py
+++ level.py
@@ -4,7 +4,6 @@ import os
 from settings import *
 from levels import LEVELS
 
-
 class Level:
 
     def __init__(self, level_number):
@@ -12,7 +11,6 @@ class Level:
         self.level_number = level_number
         self.data = LEVELS[level_number]
 
-        # Ground platform
         self.platform = pygame.Rect(
             0,
             350,
@@ -20,7 +18,6 @@ class Level:
             50
         )
 
-        # Goal area
         self.goal = pygame.Rect(
             750,
             300,
@@ -28,7 +25,6 @@ class Level:
             50
         )
 
-        # Background
         self.background = pygame.image.load(
             os.path.join(
                 ASSET_PATH,
@@ -41,7 +37,6 @@ class Level:
             (WIDTH, HEIGHT)
         )
 
-        # Coin image
         self.coin_image = pygame.image.load(
             os.path.join(
                 ASSET_PATH,
@@ -54,40 +49,34 @@ class Level:
             (20, 20)
         )
 
-        # Create coins for this level
         self.coins = []
 
         for x, y in self.data["coins"]:
 
-            coin_rect = pygame.Rect(
-                x,
-                y,
-                20,
-                20
-            )
-
             self.coins.append(
-                coin_rect
+                pygame.Rect(
+                    x,
+                    y,
+                    20,
+                    20
+                )
             )
 
         self.score = 0
 
     def draw(self, screen):
 
-        # Draw background
         screen.blit(
             self.background,
             (0, 0)
         )
 
-        # Draw platform
         pygame.draw.rect(
             screen,
             (0, 255, 0),
             self.platform
         )
 
-        # Draw coins
         for coin in self.coins:
 
             screen.blit(
@@ -95,7 +84,6 @@ class Level:
                 coin
             )
 
-        # Draw goal
         pygame.draw.rect(
             screen,
             (0, 0, 255),
@@ -108,10 +96,7 @@ class Level:
 
             if player.rect.colliderect(coin):
 
-                self.coins.remove(
-                    coin
-                )
-
+                self.coins.remove(coin)
                 self.score += 1
 
     def level_complete(self):
@@ -121,9 +106,7 @@ class Level:
     def check_goal(self, player):
 
         return (
-            player.rect.colliderect(
-                self.goal
-            )
+            player.rect.colliderect(self.goal)
             and
             self.level_complete()
         )
